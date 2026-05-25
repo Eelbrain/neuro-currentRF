54304a5407df9a0788a5fee9a8ac835af16059f4
Christian Brodbeck <christianbrodbeck@nyu.edu>
2026-05-25 14:29:51 -0400

    TEST time shift/lag equivalence (xfail)

--- ncrf/tests/test_ncrf.py
+++ ncrf/tests/test_ncrf.py
@@ -3,7 +3,10 @@
 # Author: Proloy Das <email:proloyd94@gmail.com>
 # License: BSD (3-clause)
 import pickle
+
+from eelbrain import set_time, set_tmin
 import numpy as np
+import pytest
 
 from ncrf import fit_ncrf
 from ncrf.tests.fetch import load
@@ -80,3 +83,54 @@ def test_ncrf():
     # test without multiprocessing
     model_no_mp = fit_ncrf(meg, stim, fwd, emptyroom, tstop=0.2, normalize='l1', mu='auto', n_iter=1, n_iterc=2, n_iterf=2, n_workers=0, do_post_normalization=False)
     assert_dataobj_equal(model_no_mp.h, model.h)
+
+
+@pytest.mark.xfail(
+    reason="fit_ncrf currently includes padded edge rows for shifted non-zero lags",
+    strict=True,
+)
+def test_ncrf_shifted_nonzero_lags():
+    meg = load('meg').sub(case=0)
+    stim = load('stim').sub(case=0)
+    fwd = load('fwd_sol')
+    emptyroom = load('emptyroom')
+
+    # Get a short excerpt from the test data
+    tstep = meg.get_dim('time').tstep
+    segment_start = 20 * tstep
+    segment_stop = segment_start + 80 * tstep
+    stim_segment = set_tmin(stim.sub(time=(segment_start, segment_stop)), 0)
+    meg_0 = set_time(set_tmin(meg, -segment_start), stim_segment)
+
+    # Create meg data arrays with a shift relative to the predictor
+    shift = 5 * tstep
+    lag_stop = 10 * tstep
+    meg_positive = set_time(set_tmin(meg, shift - segment_start), stim_segment)
+    meg_negative = set_time(set_tmin(meg, -shift - segment_start), stim_segment)
+
+    fit_kwargs = dict(
+        mu=0.001,
+        tol=0,
+        verbose=False,
+        n_iter=1,
+        n_iterc=1,
+        n_iterf=5,
+        do_post_normalization=False,
+    )
+    model_0 = fit_ncrf(
+        meg_0, stim_segment, fwd, emptyroom, tstart=0, tstop=lag_stop, **fit_kwargs,
+    )
+    model_positive = fit_ncrf(
+        meg_positive, stim_segment, fwd, emptyroom, tstart=shift,
+        tstop=lag_stop + shift, **fit_kwargs,
+    )
+    model_negative = fit_ncrf(
+        meg_negative, stim_segment, fwd, emptyroom, tstart=-shift,
+        tstop=lag_stop - shift, **fit_kwargs,
+    )
+
+    assert np.linalg.norm(model_0.theta) > 0
+    for model_shifted in (model_positive, model_negative):
+        np.testing.assert_allclose(model_shifted.theta, model_0.theta)
+        np.testing.assert_allclose(model_shifted.residual, model_0.residual)
+        np.testing.assert_allclose(model_shifted.explained_var, model_0.explained_var)
